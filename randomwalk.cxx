a9fc8811713c8a05898b55fc5cde0b01bb0c09f8
Saskia Funk <safunk@pc03.pool.tp1.uni-duesseldorf.de>
2015-11-24 12:04:16 +0100

    randomwalk

--- randomwalk.cxx
+++ randomwalk.cxx
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <sstream>
 #include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
@@ -11,6 +12,10 @@ struct colloid{
 
 void init(colloid* const c, const int N);
 void print(const colloid* const c, const int N, const string fname);
+void cond(int* rx, int* ry, const int N);
+void push(colloid* const c, const double step,const int N, int* rx,int* ry);
+void calc(const colloid* const c,double& meanx, double& meany, double& var,int N);
+
 
 int main(void){
   
@@ -44,7 +49,11 @@ int main(void){
     
     for(int i = 1; i <= Nfiles; i++){
 	for(int j = 0; j < Nsubsteps; j++){
-	    // call to function which randomly sets up rx and ry
+	  
+ cond (rx,ry,N);
+ push (c,step, N,rx,ry);
+ calc (c,meanx,meany, var,N);
+	      // call to function which randomly sets up rx and ry
 	    // call to function which pushes all colloids according to rx and ry
 	    // call to function which evaluates statistics
 	    stat << (i-1)*Nsubsteps+j << "\t" << meanx << "\t";
@@ -74,4 +83,44 @@ void print(const colloid* const c, const int N, const string fname){
     for(int i = 0; i < N; i++)
 	out << c[i].x << "\t" << c[i].y << endl;
     out.close();
+}
+void cond(int* rx, int* ry, const int N){
+   //int(3*double(rand()/RAND_MAX)-1)
+  for(int i = 0; i< N; i++){
+  rx[i]=(rand() % 3 - 1);
+  ry[i]=(rand() % 3 - 1);
+  }
+}
+
+void push(colloid* const c,const double step,const int N, int* rx,int* ry){
+ for(int i=0;i<N;i++){
+  c[i].x+=rx[i];	//struct deswegen .x bzw .y
+  c[i].y+=ry[i];
+ }
+ 
+
+ 
+}
+void calc(const colloid* c,double& meanx, double& meany, double& var,int N){
+    meanx=0;
+    meany=0;
+    var=0;
+    
+     for(int i=0;i<N; i++){
+     
+    meanx+=c[i].x;//meanx = meanx + c[i].x
+    meany+=c[i].y;
+    
+  }
+  //meanx=meanx/N
+ meanx/=N;
+ meany/=N; 
+ double x= 0,y =0;
+ 
+ for(int i=0; i<N; i++){
+ x = (c[i].x-meanx)*(c[i].x-meanx);
+ y = (c[i].y-meany)*(c[i].y-meany);
+ var+= (x+y);
+ }
+ var/=N;
 }
